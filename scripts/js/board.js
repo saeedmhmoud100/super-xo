97fe05e5d40719a9301cd289d0ace65a0b4d2fdc
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-02-10 12:53:43 +0200

    solve some problems and starting in the main board

--- scripts/js/board.js
+++ scripts/js/board.js
@@ -8,6 +8,8 @@ class Board {
         this.win1=-1;
         this.win2=-1;
         this.win3=-1;
+        this.is_winning=false;
+        this.winner='';
         for (let i = 0; i < 9; i++) {
             if (this.buttons) {
                 (this.buttons)[i] = -1;
@@ -15,6 +17,7 @@ class Board {
         }
         this.x_turn = true;
         this.n_move = 0;
+        this.layout=false;
         this.render_to_html();
     }
     button_click(ind) {
@@ -34,27 +37,45 @@ class Board {
         this.win3=b3;
     }
     check_winning(){
+        let b;
         if(this.buttons[0] === this.buttons[1] && this.buttons[0] === this.buttons[2] && this.buttons[2] !==-1){
             this.add_wins(0,1,2)
+            b=true;
         }else if(this.buttons[3] === this.buttons[4] && this.buttons[3] === this.buttons[5] &&this.buttons[5] !==-1){
+            b=true;
             this.add_wins(3,4,5)
         }else if(this.buttons[6] === this.buttons[7] && this.buttons[6] === this.buttons[8] &&this.buttons[8]!==-1){
+            b=true;
             this.add_wins(6,7,8)
-        }else if(this.buttons[0] === this.buttons[3] && this.buttons[0] === this.buttons[6]&& this.buttons[6]!==-1){
+        }
+        else if(this.buttons[0] === this.buttons[3] && this.buttons[0] === this.buttons[6]&& this.buttons[6]!==-1){
+            b=true;
             this.add_wins(0,3,6)
         }else if(this.buttons[1] === this.buttons[4] && this.buttons[1] === this.buttons[7]&&this.buttons[7] !==-1){
+            b=true;
             this.add_wins(1,4,7)
         }else if(this.buttons[2] === this.buttons[5] && this.buttons[2] === this.buttons[8]&& this.buttons[8]!==-1){
+            b=true;
             this.add_wins(2,5,8)
-        }else if(this.buttons[1] === this.buttons[4] && this.buttons[1] === this.buttons[8]&& this.buttons[8] !==-1){
-            this.add_wins(1,4,8)
-        }else if(this.buttons[2] === this.buttons[4] && this.buttons[2] === this.buttons[6]&& this.buttons[6]!==-1){
-            this.add_wins(2,4,6)
-        }else if(this.n_move === 9){
-            for (let i =0;i<9;i++){
-                this.buttons[i] =-1;
-            }
         }
+        else if(this.buttons[0] === this.buttons[4] && this.buttons[0] === this.buttons[8]&& this.buttons[8] !==-1){
+            b=true;
+            this.add_wins(0,4,8)
+        }else if(this.buttons[2] === this.buttons[4] && this.buttons[2] === this.buttons[6]&& this.buttons[6]!==-1){
+            b=true;
+            this.add_wins(2,4,6)
+
+        }
+        else if(this.n_move === 9 && !this.is_winning){
+            this.add_layout()
+        }
+
+        if(b){
+            this.is_winning =true;
+            this.winner = this.x_turn ? 'o' : 'x';
+            this.add_layout()
+        }
+
     }
 
     renderSymbol(index) {
@@ -62,11 +83,35 @@ class Board {
     }
 
 
+    add_layout(){
+        this.layout = true;
+        this.render_to_html()
+    }
+    remove_layout(){
+        this.layout = false;
+        this.render_to_html()
+    }
+
     render_to_html() {
         let html = '';
         for (let i =0;i<9;i++) {
             html += `<div name="${Board.obj_number+'-'+i}" class="cell ${i === this.win1 || i === this.win2 || i === this.win3 ? "win" : ""}">${this.renderSymbol(i)}</div>`;
         }
+        if(this.is_winning){
+            html+= `<h3>${this.winner +' wins'}</h3>`
+        }
+
+        if(this.n_move ===9){
+            html+= `<h3>${"no one" +' wins'}</h3>`
+        }
+
+
+        if(this.layout){
+            const lay = `<div class="layout"></div>`
+            html+=lay;
+        }
+
+
         const el = document.getElementById(this.dir_id_to_render);
 
         if (el != null) {
@@ -95,4 +140,21 @@ class Board {
 }
 
 Board.obj_number =0;
+
+/*
+*   Board{
+*       is_winning => true,false
+*       winner => 'x', 'o'
+*       n_move => number ov moves
+*       add_layout => to add layout
+*       remove_layout => to remove layout
+*       render_to_html => to rerender the html
+*   }
+*
+*
+* */
+
+
+
+
 export default  Board;
--- scripts/js/script.js
+++ scripts/js/script.js
@@ -1,4 +1,7 @@
 "use strict";
 import Board from "./board.js";
-const board1 = new Board('container');
+const board1 = new Board('board-1');
+const board2 = new Board('board-2');
+const board3 = new Board('board-3');
+const board4 = new Board('board-4');
 
